f425d3765c4dea3f2e550720278f9d424579ee5d
Steve Purcell <steve@sanityinc.com>
2019-01-08 09:18:39 +1300

    Make explicit the need for Emacs 24 (due to use of custom-push-theme)

--- default-text-scale.el
+++ default-text-scale.el
@@ -1,4 +1,4 @@
-;;; default-text-scale.el --- Easily adjust the font size in all frames
+;;; default-text-scale.el --- Easily adjust the font size in all frames -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2015  Steve Purcell
 
@@ -6,6 +6,7 @@
 ;; URL: https://github.com/purcell/default-text-scale
 ;; Keywords: frames, faces
 ;; Package-Version: 0
+;; Package-Requires: ((emacs "24"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
